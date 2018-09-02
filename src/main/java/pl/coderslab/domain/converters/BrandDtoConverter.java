5d0420e5ad264f8848949e6a97db27ea8911ff4c
Artur Siembab <ajtuss@gmail.com>
2018-09-02 16:05:09 +0200

    add BrandConverter

---
+++ src/main/java/pl/coderslab/domain/converters/BrandDtoConverter.java
@@ -0,0 +1,20 @@
+package pl.coderslab.domain.converters;
+
+import org.modelmapper.spi.MappingContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import pl.coderslab.domain.dto.BrandDto;
+import pl.coderslab.domain.services.BrandService;
+
+@Component
+public class BrandDtoConverter implements Converter<String, BrandDto> {
+
+    @Autowired
+    private BrandService brandService;
+
+    @Override
+    public BrandDto convert(String brandId) {
+        return brandService.findById(Long.parseLong(brandId));
+    }
+}
