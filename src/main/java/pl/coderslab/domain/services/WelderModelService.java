f67c85458852b4ff415fe9b97275d1f04bd603c2
Artur Siembab <ajtuss@gmail.com>
2018-09-02 16:16:34 +0200

    update Services

--- src/main/java/pl/coderslab/domain/services/WelderModelService.java
+++ src/main/java/pl/coderslab/domain/services/WelderModelService.java
@@ -1,13 +1,13 @@
 package pl.coderslab.domain.services;
 
-import pl.coderslab.domain.entities.WelderModel;
+import pl.coderslab.domain.dto.WelderModelDTO;
 
 import java.util.List;
 
 public interface WelderModelService {
 
-    WelderModel findById(Long id);
-    List<WelderModel> findAll();
-    void save(WelderModel model);
-    void update(Long id, WelderModel model);
+    WelderModelDTO findById(Long id);
+    List<WelderModelDTO> findAll();
+    void save(WelderModelDTO model);
+    void update(Long id, WelderModelDTO model);
 }
--- src/main/java/pl/coderslab/domain/services/WelderModelServiceImpl.java
+++ src/main/java/pl/coderslab/domain/services/WelderModelServiceImpl.java
@@ -1,41 +1,59 @@
 package pl.coderslab.domain.services;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.coderslab.domain.dto.WelderModelDTO;
 import pl.coderslab.domain.entities.WelderModel;
+import pl.coderslab.domain.repositories.BrandRepository;
 import pl.coderslab.domain.repositories.WelderModelRepository;
 
 import javax.transaction.Transactional;
+import java.lang.reflect.Type;
 import java.util.List;
 
 @Service
 @Transactional
 public class WelderModelServiceImpl implements WelderModelService {
 
+    private final BrandRepository brandRepository;
+
     private final WelderModelRepository modelRepository;
 
+    private final ModelMapper modelMapper;
+
     @Autowired
-    public WelderModelServiceImpl(WelderModelRepository modelRepository) {
+    public WelderModelServiceImpl(WelderModelRepository modelRepository, ModelMapper modelMapper, BrandRepository brandRepository) {
         this.modelRepository = modelRepository;
+        this.modelMapper = modelMapper;
+        this.brandRepository = brandRepository;
     }
 
     @Override
-    public WelderModel findById(Long id) {
-        return modelRepository.findById(id).orElse(null);
+    public WelderModelDTO findById(Long id) {
+        WelderModel model = modelRepository.findById(id).orElse(null);
+        return modelMapper.map(model, WelderModelDTO.class);
     }
 
     @Override
-    public List<WelderModel> findAll() {
-        return modelRepository.findAll();
+    public List<WelderModelDTO> findAll() {
+        List<WelderModel> models = modelRepository.findAll();
+        Type resultType = new TypeToken<List<WelderModelDTO>>() {}.getType();
+        return modelMapper.map(models, resultType);
     }
 
     @Override
-    public void save(WelderModel model) {
-        modelRepository.save(model);
+    public void save(WelderModelDTO modelDTO) {
+        WelderModel welderModel = modelMapper.map(modelDTO, WelderModel.class);
+        modelRepository.save(welderModel);
     }
 
     @Override
-    public void update(Long id, WelderModel model) {
-
+    public void update(Long id, WelderModelDTO modelDTO) {
+        WelderModel modelOld = modelRepository.findById(id).orElse(null);
+        WelderModel model = modelMapper.map(modelDTO, WelderModel.class);
+        //todo
+//        modelOld.setBrand();
     }
 }
