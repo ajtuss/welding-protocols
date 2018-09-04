78766e2b7b33ad0563a65b3e5a10d7b0f1ff4f69
Artur Siembab <ajtuss@gmail.com>
2018-09-04 14:57:17 +0200

    update add machine form

--- src/main/resources/static/js/addMachine.js
+++ src/main/resources/static/js/addMachine.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
     var brandSelect = $('#brand');
+    var modelSelect = $('#model');
+    var customerSelect = $('#customer');
 
     function setBrandsToSelect() {
         $.get('/api/brands')
@@ -12,15 +14,35 @@ $(document).ready(function () {
             });
     }
 
-    function setModelsToSelect() {
-        $.get('/api/model')
+    function setCustomersToSelect() {
+        $.get('/api/customers')
             .done(function (res) {
-                $(res).each(function (index, brand) {
-                    var select = $("<option>").val(brand.id).text(brand.name);
-                    brandSelect.append(select);
+                $(res).each(function (index, customer) {
+                    var select = $("<option>").val(customer.id).text(customer.shortName);
+                    customerSelect.append(select);
                 });
             });
     }
 
+    var setModelsToSelect = function () {
+        var brandId = brandSelect.val();
+        modelSelect.empty();
+        if (brandId !== "") {
+            $.get('/api/brands/' + brandSelect.val() + '/models')
+                .done(function (res) {
+                    $(res).each(function (index, model) {
+                        var select = $("<option>").val(model.id).text(model.name);
+                        modelSelect.append(select);
+                    });
+                });
+        }
+    };
+
     setBrandsToSelect();
+    setCustomersToSelect();
+
+    brandSelect.change(function () {
+        // brandSelect.find('[value=""]').remove();
+        setModelsToSelect();
+    });
 });
