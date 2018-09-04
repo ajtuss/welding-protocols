56ea32dbc3e38e4ca075e7d15fa52dab6ee88664
Artur Siembab <ajtuss@gmail.com>
2018-09-04 11:59:36 +0200

    add CustomerRestController

---
+++ src/main/java/pl/coderslab/web/rest/CustomerRestController.java
@@ -0,0 +1,27 @@
+package pl.coderslab.web.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.domain.dto.CustomerDto;
+import pl.coderslab.domain.services.CustomerService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customer")
+public class CustomerRestController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerRestController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<CustomerDto> getAllCustomers(){
+        return customerService.findAll();
+    }
+}
