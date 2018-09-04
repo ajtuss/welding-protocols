3c10b9a596707994c9a23a042c96224234bf4b7d
Artur Siembab <ajtuss@gmail.com>
2018-09-04 10:28:14 +0200

    delete adnotation

--- src/main/java/pl/coderslab/domain/repositories/RoleRepository.java
+++ src/main/java/pl/coderslab/domain/repositories/RoleRepository.java
@@ -1,10 +1,8 @@
 package pl.coderslab.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import pl.coderslab.domain.entities.Role;
 
-@Repository("roleRepository")
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Role findByRole(String role);
 
