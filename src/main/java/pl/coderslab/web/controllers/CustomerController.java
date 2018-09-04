56f6db014e7a5b647440139cff00defc539ca23d
Artur Siembab <ajtuss@gmail.com>
2018-09-04 11:59:45 +0200

    add CustomerController

---
+++ src/main/java/pl/coderslab/web/controllers/CustomerController.java
@@ -0,0 +1,25 @@
+package pl.coderslab.web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.domain.services.CustomerService;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public String showCustomers(){
+
+        return "customers";
+    }
+}
