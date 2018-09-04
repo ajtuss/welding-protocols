00e2ebb7fbaabdebbbac9c692d42432de3d7afa3
Artur Siembab <ajtuss@gmail.com>
2018-09-04 11:57:10 +0200

    add customer view

---
+++ src/main/resources/static/js/customers.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+    var checked = '<span data-feather="check-square"></span>';
+    var unChecked = '<span data-feather="square"></span>';
+
+    var table = $('#table').DataTable({
+        initComplete: function () {
+            feather.replace();
+        },
+
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                text: 'Dodaj',
+                className: '',
+                action: function (e, dt, button, config) {
+                    window.location.href = '/customer/add';
+                }
+
+            },
+            {
+                extend: 'selectedSingle',
+                text: 'Edytuj',
+                action: function (e, dt, button, config) {
+                    window.location.href = '/customer/' + dt.row({selected: true}).data().id;
+                }
+            },
+            {
+                extend: 'selectedSingle',
+                text: 'Usuń',
+                action: function (e, dt, button, config) {
+                    window.location.href = '/customer/' + dt.row({selected: true}).data().id + '/delete';
+                }
+            }
+        ],
+        paging: true,
+        select: {
+            style: 'single'
+        },
+        ajax: {
+            url: '/api/customer',
+            type: 'get', //typ połączenia
+            // contentType: 'application/json', //gdy wysyłamy dane czasami chcemy ustawić ich typ
+            // dataType: 'json', //typ danych jakich oczekujemy w odpowiedzi
+            dataSrc: ""
+            // success: function (res) {
+            //     // console.log(res);
+            // }
+        },
+        columns: [
+            {
+                'data': 'id',
+                'width': '5%'
+            },
+            {'data': 'shortName'},
+            {
+                'data': 'fullName'
+            },
+            {
+                'data': 'nip'
+            },
+            {
+                'data': 'zip',
+                'width': '7%'
+            },
+            {
+                'data': 'city',
+                'width': '7%'
+            },
+            {
+                'data': 'street',
+                'width': '7%'
+            },
+            {
+                'data': 'email',
+                'width': '7%'
+            }
+
+        ]
+
+    });
+
+});
