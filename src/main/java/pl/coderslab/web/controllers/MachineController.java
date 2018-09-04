5768bc89a957b8cf2244a7333196fe5f29554db1
Artur Siembab <ajtuss@gmail.com>
2018-09-04 16:55:44 +0200

    update machine controller

--- src/main/java/pl/coderslab/web/controllers/MachineController.java
+++ src/main/java/pl/coderslab/web/controllers/MachineController.java
@@ -3,8 +3,7 @@ package pl.coderslab.web.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.domain.dto.MachineDto;
 import pl.coderslab.domain.services.MachineService;
 
@@ -28,5 +27,24 @@ public class MachineController {
     public String showAddMachineForm(Model model){
         model.addAttribute("machine", new MachineDto());
         return "forms/addMachine";
+    }
+
+    @PostMapping("/add")
+    public String addMachine(@ModelAttribute MachineDto machineDto){
+        machineService.save(machineDto);
+        return "redirect:/machines";
+    }
+
+    @GetMapping("{id:\\d+}")
+    public String showEditMachineForm(@PathVariable Long id, Model model){
+        MachineDto machineDto = machineService.findById(id);
+        model.addAttribute("machine", machineDto);
+        return "forms/editMachine";
+    }
+
+    @PostMapping("{id:\\d+}")
+    public String editMachine(@PathVariable Long id, @ModelAttribute MachineDto machineDto){
+        machineService.update(id, machineDto);
+        return "redirect:/machines";
     }
 }
