7052a6eba277b2a54c722a2a34a93fa7d1628474
Artur Siembab <ajtuss@gmail.com>
2018-09-07 11:10:26 +0200

    add tests

---
+++ src/test/java/pl/coderslab/domain/repositories/BrandRepositoryTest.java
@@ -0,0 +1,44 @@
+package pl.coderslab.domain.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.coderslab.config.Application;
+import pl.coderslab.domain.entities.Brand;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class})
+@DataJpaTest
+public class BrandRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    private Brand brand1;
+
+    @Before
+    public void setUp(){
+        brand1 = new Brand();
+        brand1.setName("Brand name");
+        entityManager.persist(brand1);
+        entityManager.flush();
+    }
+
+    @Test
+    public void expectedTrueAfterFindByName(){
+
+        Brand found = brandRepository.findByName(brand1.getName());
+        assertEquals(found.getName(), brand1.getName() );
+    }
+
+}
---
+++ src/test/java/pl/coderslab/domain/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,51 @@
+package pl.coderslab.domain.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.coderslab.config.Application;
+import pl.coderslab.domain.entities.Customer;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class})
+@DataJpaTest
+public class CustomerRepositoryTest {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    private Customer customer1;
+
+    @Before
+    public void setUp(){
+        customer1 = new Customer();
+        customer1.setShortName("short name");
+        customer1.setFullName("full name");
+        customer1.setZip("00-000");
+        customer1.setCity("city");
+        customer1.setStreet("street 23");
+        customer1.setEmail("email@email.com");
+        customer1.setNip("1250249764");
+        entityManager.persist(customer1);
+        entityManager.flush();
+    }
+
+    @Test
+    public void expectedTrueAfterFindByShortName(){
+        Customer found = customerRepository.findByShortName(customer1.getShortName());
+        assertEquals(customer1, found);
+    }
+
+}
