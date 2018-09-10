756f475e5d5fddd54eff708bc014ee5718c6a22c
Artur Siembab <ajtuss@gmail.com>
2018-09-10 16:21:40 +0200

    delete unused converter

--- src/main/java/pl/coderslab/config/AppConfig.java
+++ src/main/java/pl/coderslab/config/AppConfig.java
@@ -3,16 +3,11 @@ package pl.coderslab.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-    }
 
     @Bean
     public ModelMapper modelMapper() {
