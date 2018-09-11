a5eb350334e37836a4372e93f8fe9dd4712d6c32
Artur Siembab <ajtuss@gmail.com>
2018-09-11 12:49:14 +0200

    fix getOne method

--- src/main/java/pl/coderslab/web/rest/WelderModelRestController.java
+++ src/main/java/pl/coderslab/web/rest/WelderModelRestController.java
@@ -1,35 +1,55 @@
 package pl.coderslab.web.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.*;
+import pl.coderslab.domain.dto.BrandDTO;
+import pl.coderslab.domain.dto.MachineDTO;
 import pl.coderslab.domain.dto.WelderModelDTO;
 import pl.coderslab.domain.services.WelderModelService;
+import pl.coderslab.web.rest.assemblers.WelderModelResourceAssembler;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/models")
+@RequestMapping(value = "/api/models", consumes = MediaTypes.HAL_JSON_UTF8_VALUE, produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class WelderModelRestController {
 
     private final WelderModelService modelService;
 
+    private final WelderModelResourceAssembler assembler;
+
     @Autowired
-    public WelderModelRestController(WelderModelService modelService) {
+    public WelderModelRestController(WelderModelService modelService, WelderModelResourceAssembler assembler) {
         this.modelService = modelService;
+        this.assembler = assembler;
     }
 
     @GetMapping
-    public List<WelderModelDTO> getAllModels() {
+    public List<WelderModelDTO> getAll() {
         return modelService.findAll();
     }
 
     @GetMapping("{id:\\d+}")
-    public WelderModelDTO getModel(@PathVariable Long id) {
-        return modelService.findById(id);
+    public Resource<WelderModelDTO> getOne(@PathVariable Long id) {
+        WelderModelDTO modelDTO = modelService.findById(id);
+        return assembler.toResource(modelDTO);
     }
 
     @DeleteMapping("{id:\\d+}")
     public void deleteModel(@PathVariable Long id) {
         modelService.remove(id);
+    }
+
+    @GetMapping("{id:\\d+}/brands")
+    public Resource<BrandDTO> getBrandByModelId(@PathVariable Long id){
+        return null;
+    }
+
+    @GetMapping("{id:\\d+}/machines")
+    public Resources<Resource<MachineDTO>> getMachinesByModelId(@PathVariable Long id){
+        return null;
     }
 }
