a7a5f30829f90ab840e3c4b9f98e9fd721f1c64e
Artur Siembab <ajtuss@gmail.com>
2018-09-11 09:21:14 +0200

    refactor tests

--- src/test/java/pl/coderslab/web/rest/BrandRestControllerTest.java
+++ src/test/java/pl/coderslab/web/rest/BrandRestControllerTest.java
@@ -35,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Import({BrandResourceAssembler.class})
 public class BrandRestControllerTest {
 
+    public static final BrandUpdateDTO BRAND_UPDATE_1 = new BrandUpdateDTO(1L, "Kemppi", 1L);
     @Autowired
     private MockMvc mockMvc;
 
@@ -44,13 +45,15 @@ public class BrandRestControllerTest {
     private static final ObjectMapper mapper = new ObjectMapper();
 
     private static final LocalDateTime DATE_TIME = LocalDateTime.now();
+    private static final BrandDTO BRAND_1 = new BrandDTO(1L, "Kemppi", DATE_TIME, DATE_TIME, 1L);
+    private static final BrandDTO BRAND_2 = new BrandDTO(2L, "Fronius", DATE_TIME, DATE_TIME, 1L);
+    private static final BrandDTO BRAND_1_AFTER_UPDATE = new BrandDTO(1L, "Kemppi", DATE_TIME, DATE_TIME, 2L);
 
     @Test
     public void getShouldFetchAllAHalDocument() throws Exception {
 
         given(brandService.findAll()).willReturn(
-                Arrays.asList(new BrandDTO(1L, "Kemppi", DATE_TIME, DATE_TIME, 1L),
-                        new BrandDTO(2L, "Fronius", DATE_TIME, DATE_TIME, 1L))
+                Arrays.asList(BRAND_1, BRAND_2)
         );
 
         mockMvc.perform(get("/api/brands")
@@ -58,19 +61,19 @@ public class BrandRestControllerTest {
                .andDo(print())
                .andExpect(status().isOk())
                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-               .andExpect(jsonPath("$._embedded.brands[0].id", is(1)))
-               .andExpect(jsonPath("$._embedded.brands[0].name", is("Kemppi")))
+               .andExpect(jsonPath("$._embedded.brands[0].id", is(BRAND_1.getId().intValue())))
+               .andExpect(jsonPath("$._embedded.brands[0].name", is(BRAND_1.getName())))
                .andExpect(jsonPath("$._embedded.brands[0].creationDate", is(notNullValue())))
                .andExpect(jsonPath("$._embedded.brands[0].modificationDate", is(notNullValue())))
-               .andExpect(jsonPath("$._embedded.brands[0].versionId", is(1)))
+               .andExpect(jsonPath("$._embedded.brands[0].versionId", is(BRAND_1.getVersionId().intValue())))
                .andExpect(jsonPath("$._embedded.brands[0]._links.self.href", is("http://localhost/api/brands/1")))
                .andExpect(jsonPath("$._embedded.brands[0]._links.brands.href", is("http://localhost/api/brands")))
                .andExpect(jsonPath("$._embedded.brands[0]._links.models.href", is("http://localhost/api/brands/1/models")))
-               .andExpect(jsonPath("$._embedded.brands[1].id", is(2)))
-               .andExpect(jsonPath("$._embedded.brands[1].name", is("Fronius")))
+               .andExpect(jsonPath("$._embedded.brands[1].id", is(BRAND_2.getId().intValue())))
+               .andExpect(jsonPath("$._embedded.brands[1].name", is(BRAND_2.getName())))
                .andExpect(jsonPath("$._embedded.brands[1].creationDate", is(notNullValue())))
                .andExpect(jsonPath("$._embedded.brands[1].modificationDate", is(notNullValue())))
-               .andExpect(jsonPath("$._embedded.brands[1].versionId", is(1)))
+               .andExpect(jsonPath("$._embedded.brands[1].versionId", is(BRAND_2.getVersionId().intValue())))
                .andExpect(jsonPath("$._embedded.brands[1]._links.self.href", is("http://localhost/api/brands/2")))
                .andExpect(jsonPath("$._embedded.brands[1]._links.brands.href", is("http://localhost/api/brands")))
                .andExpect(jsonPath("$._embedded.brands[1]._links.models.href", is("http://localhost/api/brands/2/models")))
@@ -83,18 +86,18 @@ public class BrandRestControllerTest {
     @Test
     public void getShouldFetchAHalDocument() throws Exception {
 
-        given(brandService.findById(1L)).willReturn(new BrandDTO(1L, "Kemppi", DATE_TIME, DATE_TIME, 1L));
+        given(brandService.findById(1L)).willReturn(BRAND_1);
 
         mockMvc.perform(get("/api/brands/1")
                 .contentType(MediaTypes.HAL_JSON_UTF8_VALUE))
                .andDo(print())
                .andExpect(status().isOk())
                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-               .andExpect(jsonPath("$.id", is(1)))
-               .andExpect(jsonPath("$.name", is("Kemppi")))
+               .andExpect(jsonPath("$.id", is(BRAND_1.getId().intValue())))
+               .andExpect(jsonPath("$.name", is(BRAND_1.getName())))
                .andExpect(jsonPath("$.creationDate", is(notNullValue())))
                .andExpect(jsonPath("$.modificationDate", is(notNullValue())))
-               .andExpect(jsonPath("$.versionId", is(1)))
+               .andExpect(jsonPath("$.versionId", is(BRAND_1.getVersionId().intValue())))
                .andExpect(jsonPath("$._links.self.href", is("http://localhost/api/brands/1")))
                .andExpect(jsonPath("$._links.brands.href", is("http://localhost/api/brands")))
                .andExpect(jsonPath("$._links.models.href", is("http://localhost/api/brands/1/models")))
@@ -105,11 +108,10 @@ public class BrandRestControllerTest {
 
     @Test
     public void postShouldCreateNewBrandAndFetchAHalDocument() throws Exception {
-        BrandCreationDTO creationDTO = new BrandCreationDTO("Kemppi");
-        BrandDTO brandDTO = new BrandDTO(1L, "Kemppi", DATE_TIME, DATE_TIME, 1L);
+        BrandCreationDTO creationDTO = new BrandCreationDTO(BRAND_1.getName());
         String contentBody = mapper.writeValueAsString(creationDTO);
 
-        given(brandService.saveBrand(creationDTO)).willReturn(brandDTO);
+        given(brandService.saveBrand(creationDTO)).willReturn(BRAND_1);
         mockMvc.perform(post("/api/brands")
                 .accept(MediaTypes.HAL_JSON_UTF8_VALUE)
                 .contentType(MediaTypes.HAL_JSON_UTF8_VALUE)
@@ -117,11 +119,11 @@ public class BrandRestControllerTest {
                .andDo(print())
                .andExpect(status().isCreated())
                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-               .andExpect(jsonPath("$.id", is(1)))
-               .andExpect(jsonPath("$.name", is("Kemppi")))
+               .andExpect(jsonPath("$.id", is(BRAND_1.getId().intValue())))
+               .andExpect(jsonPath("$.name", is(BRAND_1.getName())))
                .andExpect(jsonPath("$.creationDate", is(notNullValue())))
                .andExpect(jsonPath("$.modificationDate", is(notNullValue())))
-               .andExpect(jsonPath("$.versionId", is(1)))
+               .andExpect(jsonPath("$.versionId", is(BRAND_1.getVersionId().intValue())))
                .andExpect(jsonPath("$._links.self.href", is("http://localhost/api/brands/1")))
                .andExpect(jsonPath("$._links.brands.href", is("http://localhost/api/brands")))
                .andExpect(jsonPath("$._links.models.href", is("http://localhost/api/brands/1/models")))
@@ -132,12 +134,10 @@ public class BrandRestControllerTest {
 
     @Test
     public void putShouldUpdateBrandAndFetchHalDocument() throws Exception {
-        BrandUpdateDTO updateDTO = new BrandUpdateDTO(1L, "Kemppi", 1L);
-        BrandDTO brandDTO = new BrandDTO(1L, "Kemppi", DATE_TIME, DATE_TIME, 2L);
-        String contentBody = mapper.writeValueAsString(updateDTO);
+        String contentBody = mapper.writeValueAsString(BRAND_UPDATE_1);
 
 
-        given(brandService.updateBrand(updateDTO)).willReturn(brandDTO);
+        given(brandService.updateBrand(BRAND_UPDATE_1)).willReturn(BRAND_1_AFTER_UPDATE);
         mockMvc.perform(put("/api/brands/1")
                 .accept(MediaTypes.HAL_JSON_UTF8_VALUE)
                 .contentType(MediaTypes.HAL_JSON_UTF8_VALUE)
@@ -145,27 +145,25 @@ public class BrandRestControllerTest {
                .andDo(print())
                .andExpect(status().isOk())
                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-               .andExpect(jsonPath("$.id", is(1)))
-               .andExpect(jsonPath("$.name", is("Kemppi")))
+               .andExpect(jsonPath("$.id", is(BRAND_1_AFTER_UPDATE.getId().intValue())))
+               .andExpect(jsonPath("$.name", is(BRAND_1_AFTER_UPDATE.getName())))
                .andExpect(jsonPath("$.creationDate", is(notNullValue())))
                .andExpect(jsonPath("$.modificationDate", is(notNullValue())))
-               .andExpect(jsonPath("$.versionId", is(2)))
+               .andExpect(jsonPath("$.versionId", is(BRAND_1_AFTER_UPDATE.getVersionId().intValue())))
                .andExpect(jsonPath("$._links.self.href", is("http://localhost/api/brands/1")))
                .andExpect(jsonPath("$._links.brands.href", is("http://localhost/api/brands")))
                .andExpect(jsonPath("$._links.models.href", is("http://localhost/api/brands/1/models")))
                .andReturn();
-        verify(brandService, times(1)).updateBrand(updateDTO);
+        verify(brandService, times(1)).updateBrand(BRAND_UPDATE_1);
         verifyNoMoreInteractions(brandService);
     }
 
     @Test
     public void putWithInvalidIdShouldReturnHttpStatus400() throws Exception {
-        BrandUpdateDTO updateDTO = new BrandUpdateDTO(1L, "Kemppi", 1L);
-        BrandDTO brandDTO = new BrandDTO(1L, "Kemppi", DATE_TIME, DATE_TIME, 2L);
-        String contentBody = mapper.writeValueAsString(updateDTO);
+        String contentBody = mapper.writeValueAsString(BRAND_UPDATE_1);
 
 
-        given(brandService.updateBrand(updateDTO)).willReturn(brandDTO);
+        given(brandService.updateBrand(BRAND_UPDATE_1)).willReturn(BRAND_1_AFTER_UPDATE);
         mockMvc.perform(put("/api/brands/2")
                 .accept(MediaTypes.HAL_JSON_UTF8_VALUE)
                 .contentType(MediaTypes.HAL_JSON_UTF8_VALUE)
