9e2e3b532fa6d3dfcb1fa5e86b511561815e76b9
Artur Siembab <ajtuss@gmail.com>
2018-09-11 12:48:55 +0200

    add assembler

--- src/main/java/pl/coderslab/web/rest/assemblers/WelderModelResourceAssembler.java
+++ src/main/java/pl/coderslab/web/rest/assemblers/WelderModelResourceAssembler.java
@@ -1,4 +1,20 @@
 package pl.coderslab.web.rest.assemblers;
 
-public class WelderModelResourceAssembler {
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import pl.coderslab.domain.dto.WelderModelDTO;
+import pl.coderslab.web.rest.WelderModelRestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+public class WelderModelResourceAssembler implements ResourceAssembler<WelderModelDTO, Resource<WelderModelDTO>> {
+    @Override
+    public Resource<WelderModelDTO> toResource(WelderModelDTO modelDTO) {
+        return new Resource<>(modelDTO,
+                linkTo(methodOn(WelderModelRestController.class).getOne(modelDTO.getId())).withSelfRel(),
+                linkTo(methodOn(WelderModelRestController.class).getAll()).withRel("models"),
+                linkTo(methodOn(WelderModelRestController.class).getBrandByModelId(modelDTO.getId())).withRel("brands"),
+                linkTo(methodOn(WelderModelRestController.class).getMachinesByModelId(modelDTO.getId())).withRel("machines"));
+    }
 }
