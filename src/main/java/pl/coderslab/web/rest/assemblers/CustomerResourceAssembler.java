03c5960633253cd9bd4de6333b0f628d5da0ebfd
Artur Siembab <ajtuss@gmail.com>
2018-09-13 10:30:51 +0200

    add customer resource assembler

---
+++ src/main/java/pl/coderslab/web/rest/assemblers/CustomerResourceAssembler.java
@@ -0,0 +1,20 @@
+package pl.coderslab.web.rest.assemblers;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+import pl.coderslab.domain.dto.CustomerDTO;
+import pl.coderslab.web.rest.CustomerRestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@Component
+public class CustomerResourceAssembler implements ResourceAssembler<CustomerDTO, Resource<CustomerDTO>> {
+    @Override
+    public Resource<CustomerDTO> toResource(CustomerDTO customerDTO) {
+        return new Resource<>(customerDTO,
+                linkTo(methodOn(CustomerRestController.class).getOne(customerDTO.getId())).withSelfRel(),
+                linkTo(methodOn(CustomerRestController.class).getMachineByCustomerId(customerDTO.getId())).withRel("machines"));
+    }
+}
