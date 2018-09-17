8d0feaeec00bce5f6d95fae99df4b3ed3e621b9a
Artur Siembab <ajtuss@gmail.com>
2018-09-17 12:58:00 +0200

    add sql

---
+++ src/test/resources/data-machines.sql
@@ -0,0 +1,7 @@
+INSERT INTO `ranges` VALUES (8,250,10,30,20.5),(9,300,5,22,10),(10,350,10,34,20.5),(11,400,5,26,10);
+
+INSERT INTO `models` VALUES
+(2,'2018-09-14 11:47:21','2018-09-17 12:10:59',2, false , false , true, 'MasterTig 3000', false , true , false ,2,NULL,8,9),
+(3,'2018-09-14 11:47:31','2018-09-17 12:13:47',2, false , false , true,'MasterTig 4000', false , true , false ,2,NULL,10,11);
+
+INSERT INTO `customers` VALUES (1,'2018-09-17 12:20:17','2018-09-17 12:20:17',1,'Katowice',NULL,'Firma sp. z O.O.',NULL,'Firma','Kościuszki 18','40-584');
