2af11d515105d6b57d56870bfa24c7ae8845b1ad
Artur Siembab <ajtuss@gmail.com>
2018-09-18 15:12:57 +0200

    add assembler

---
+++ src/main/java/pl/coderslab/web/rest/assemblers/MeasureResourceAssembler.java
@@ -0,0 +1,14 @@
+package pl.coderslab.web.rest.assemblers;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+import pl.coderslab.domain.dto.MeasureDTO;
+
+@Component
+public class MeasureResourceAssembler implements ResourceAssembler<MeasureDTO, Resource<MeasureDTO>> {
+    @Override
+    public Resource<MeasureDTO> toResource(MeasureDTO entity) {
+        return null; //todo
+    }
+}
