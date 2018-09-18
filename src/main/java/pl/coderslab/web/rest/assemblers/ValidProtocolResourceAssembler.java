33521eb8d7754833204894e0aff97ad3819f6345
Artur Siembab <ajtuss@gmail.com>
2018-09-18 15:13:02 +0200

    add assembler

---
+++ src/main/java/pl/coderslab/web/rest/assemblers/ValidProtocolResourceAssembler.java
@@ -0,0 +1,25 @@
+package pl.coderslab.web.rest.assemblers;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+import pl.coderslab.domain.dto.ValidProtocolDTO;
+import pl.coderslab.web.rest.ValidationsRestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+
+@Component
+public class ValidProtocolResourceAssembler implements ResourceAssembler<ValidProtocolDTO, Resource<ValidProtocolDTO>> {
+    @Override
+    public Resource<ValidProtocolDTO> toResource(ValidProtocolDTO protocolDTO) {
+        return new Resource<>(protocolDTO,
+                linkTo(methodOn(ValidationsRestController.class).getOne(protocolDTO.getId())).withSelfRel(),
+                linkTo(methodOn(ValidationsRestController.class).getAll()).withRel("validations"),
+                linkTo(methodOn(ValidationsRestController.class).getMeasures(protocolDTO.getId())).withRel("measures"),
+                linkTo(methodOn(ValidationsRestController.class).getMachines(protocolDTO.getId())).withRel("machines"),
+                linkTo(methodOn(ValidationsRestController.class).getCustomers(protocolDTO.getId())).withRel("customers"),
+                linkTo(methodOn(ValidationsRestController.class).getModels(protocolDTO.getId())).withRel("models"));
+    }
+}
