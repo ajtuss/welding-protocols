8f6686f281ee9ae84fa1eb6e4b0ad159d1e9bf59
Artur Siembab <ajtuss@gmail.com>
2018-09-19 17:18:58 +0200

    add methods

--- src/main/java/pl/coderslab/rest/MeasureRestController.java
+++ src/main/java/pl/coderslab/rest/MeasureRestController.java
@@ -5,16 +5,16 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.domain.dto.MeasureDTO;
 import pl.coderslab.domain.dto.ValidProtocolDTO;
+import pl.coderslab.domain.exceptions.InvalidRequestException;
 import pl.coderslab.domain.services.MeasureService;
 import pl.coderslab.rest.assemblers.MeasureResourceAssembler;
 import pl.coderslab.rest.assemblers.ValidProtocolResourceAssembler;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -40,9 +40,9 @@ public class MeasureRestController {
     @GetMapping
     public Resources<Resource<MeasureDTO>> getAll() {
         List<Resource<MeasureDTO>> resources = measureService.findAll()
-                                                           .stream()
-                                                           .map(assembler::toResource)
-                                                           .collect(Collectors.toList());
+                                                             .stream()
+                                                             .map(assembler::toResource)
+                                                             .collect(Collectors.toList());
         return new Resources<>(resources,
                 linkTo(methodOn(MeasureRestController.class).getAll()).withSelfRel());
     }
@@ -57,5 +57,28 @@ public class MeasureRestController {
     public Resource<ValidProtocolDTO> getValidProtocol(@PathVariable Long id) {
         ValidProtocolDTO protocol = measureService.findProtocolByMeasureId(id);
         return protocolAssembler.toResource(protocol);
+    }
+
+    @PostMapping
+    public ResponseEntity<Resource<MeasureDTO>> addMeasure(@RequestBody @Valid MeasureDTO measureDTO) {
+        MeasureDTO save = measureService.save(measureDTO);
+        Resource<MeasureDTO> resource = assembler.toResource(save);
+        return ResponseEntity.created(linkTo(methodOn(MeasureRestController.class).getOne(save.getId())).toUri())
+                             .body(resource);
+    }
+
+    @PutMapping("/{id:\\d+}")
+    public Resource<MeasureDTO> updateMeasure(@RequestBody @Valid MeasureDTO measureDTO, @PathVariable Long id) {
+        if (!id.equals(measureDTO.getId())) {
+            throw new InvalidRequestException();
+        }
+        MeasureDTO update = measureService.update(measureDTO);
+        return assembler.toResource(update);
+    }
+
+    @DeleteMapping("/{id:\\d+}")
+    public ResponseEntity<?> deleteMeasure(@PathVariable Long id) {
+        measureService.remove(id);
+        return ResponseEntity.noContent().build();
     }
 }
