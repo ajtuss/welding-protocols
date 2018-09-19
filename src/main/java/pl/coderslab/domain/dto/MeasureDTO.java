637d32234c02649fd933d4e57de33776976a659e
Artur Siembab <ajtuss@gmail.com>
2018-09-19 17:17:25 +0200

    add validation

--- src/main/java/pl/coderslab/domain/dto/MeasureDTO.java
+++ src/main/java/pl/coderslab/domain/dto/MeasureDTO.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.hateoas.core.Relation;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -18,8 +20,13 @@ public class MeasureDTO {
 
 
     private Long id;
+    @NotNull
     private Long validProtocolId;
+
+    @NotNull
+    @DecimalMax("1000")
     private BigDecimal iAdjust;
+
     private BigDecimal uAdjust;
     private BigDecimal iPower;
     private BigDecimal uPower;
