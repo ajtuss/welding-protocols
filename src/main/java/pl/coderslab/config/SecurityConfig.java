ab78d8580e608408ec1939dfb905380e4af04d4f
Artur Siembab <ajtuss@gmail.com>
2018-09-24 09:03:19 +0200

    authorization update

--- src/main/java/pl/coderslab/config/SecurityConfig.java
+++ src/main/java/pl/coderslab/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -51,20 +53,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/css/*").permitAll()
-            .antMatchers("/webjars/*").permitAll()
-            .antMatchers("/register").anonymous()
-            .antMatchers("/login").anonymous()
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //            .antMatchers("/brands").hasAuthority("USER") // example for role authorization
-//            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login").failureUrl("/login?error=true")
-            .and()
-            .logout().logoutUrl("/logout").logoutSuccessUrl("/login")
-            .permitAll()
-            .and()
-            .csrf().disable();
     }
 }
