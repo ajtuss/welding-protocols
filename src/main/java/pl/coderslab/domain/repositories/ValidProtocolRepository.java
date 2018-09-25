f7e752cae53f2c8566e7aab8178f1983ba3756bf
Artur Siembab <ajtuss@gmail.com>
2018-09-25 10:37:11 +0200

    add Queries

--- src/main/java/pl/coderslab/domain/repositories/ValidProtocolRepository.java
+++ src/main/java/pl/coderslab/domain/repositories/ValidProtocolRepository.java
@@ -1,7 +1,13 @@
 package pl.coderslab.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.domain.entities.ValidProtocol;
 
+import java.util.List;
+
 public interface ValidProtocolRepository extends JpaRepository<ValidProtocol, Long> {
+
+    @Query("SELECT v FROM ValidProtocol v JOIN v.machine m WHERE m.id = ?1")
+    List<ValidProtocol> findByMachineId(long id);
 }
