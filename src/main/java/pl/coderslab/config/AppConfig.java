b794e8cd59aa5e8a747233ae596a5f2a6e8387d6
Artur Siembab <ajtuss@gmail.com>
2018-10-09 17:27:46 +0200

    add jhipster framework

--- src/main/java/pl/coderslab/config/AppConfig.java
+++ src/main/java/pl/coderslab/config/AppConfig.java
@@ -3,6 +3,7 @@ package pl.coderslab.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
