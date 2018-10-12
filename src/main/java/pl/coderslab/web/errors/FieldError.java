df1881dafa52ca5945200aab27359281881b7271
Artur Siembab <ajtuss@gmail.com>
2018-10-12 09:42:27 +0200

    update field error

--- src/main/java/pl/coderslab/web/errors/FieldError.java
+++ src/main/java/pl/coderslab/web/errors/FieldError.java
@@ -1,5 +1,7 @@
 package pl.coderslab.web.errors;
 
+import java.util.Objects;
+
 public class FieldError {
 
     private final String objectName;
@@ -24,5 +26,30 @@ public class FieldError {
 
     public String getMessage() {
         return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FieldError that = (FieldError) o;
+        return Objects.equals(objectName, that.objectName) &&
+                Objects.equals(field, that.field) &&
+                Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(objectName, field, message);
+    }
+
+    @Override
+    public String toString() {
+        return "FieldError{" +
+                "objectName='" + objectName + '\'' +
+                ", field='" + field + '\'' +
+                ", message='" + message + '\'' +
+                '}';
     }
 }
