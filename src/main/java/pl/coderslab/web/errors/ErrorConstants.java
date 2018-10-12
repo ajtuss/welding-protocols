755b71cda649eb3c1c9371f09633d9d9202ad135
Artur Siembab <ajtuss@gmail.com>
2018-10-12 09:43:06 +0200

    add error constants

---
+++ src/main/java/pl/coderslab/web/errors/ErrorConstants.java
@@ -0,0 +1,11 @@
+package pl.coderslab.web.errors;
+
+public final class ErrorConstants {
+
+    public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
+    public static final String ERR_VALIDATION = "error.validation";
+    public static final String ERR_IDNULL = "error.idnull";
+
+    private ErrorConstants() {
+    }
+}
