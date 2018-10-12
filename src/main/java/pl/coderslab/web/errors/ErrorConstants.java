ab4d91f2489873ae9daefefc86b5ca2c391f6333
Artur Siembab <ajtuss@gmail.com>
2018-10-12 10:53:58 +0200

    update error constants

--- src/main/java/pl/coderslab/web/errors/ErrorConstants.java
+++ src/main/java/pl/coderslab/web/errors/ErrorConstants.java
@@ -4,7 +4,8 @@ public final class ErrorConstants {
 
     public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
     public static final String ERR_VALIDATION = "error.validation";
-    public static final String ERR_IDNULL = "error.idnull";
+    public static final String ERR_ID_NULL = "error.idnull";
+    public static final String ERR_ID_EXIST = "error.idexist";
     public static final String ERR_NOT_FOUND = "error.notfound";
 
     private ErrorConstants() {
