993927911480a765de8213876a6c667f8853de2e
Artur Siembab <ajtuss@gmail.com>
2018-10-15 16:48:22 +0200

    add test sqls

---
+++ src/test/resources/data-models.sql
@@ -0,0 +1,3 @@
+INSERT INTO `models` VALUES
+(2,'2018-09-14 11:47:21','2018-09-17 12:10:59',2, false , false , true, 'MasterTig 3000', false , true , false ,2,NULL,8,9),
+(3,'2018-09-14 11:47:31','2018-09-17 12:13:47',2, false , false , true,'MasterTig 4000', false , true , false ,2,NULL,10,11);
