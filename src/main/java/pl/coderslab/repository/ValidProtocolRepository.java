1cda731674598c6713f20322cfd3210779a35a87
Artur Siembab <ajtuss@gmail.com>
2018-10-26 10:07:23 +0200

    add pageable

--- src/main/java/pl/coderslab/repository/ValidProtocolRepository.java
+++ src/main/java/pl/coderslab/repository/ValidProtocolRepository.java
@@ -1,13 +1,13 @@
 package pl.coderslab.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.domain.ValidProtocol;
 
-import java.util.List;
-
 public interface ValidProtocolRepository extends JpaRepository<ValidProtocol, Long> {
 
     @Query("SELECT v FROM ValidProtocol v JOIN v.machine m WHERE m.id = ?1")
-    List<ValidProtocol> findByMachineId(long id);
+    Page<ValidProtocol> findByMachineId(long id, Pageable pageable);
 }
